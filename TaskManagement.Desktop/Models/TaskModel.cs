7feb4b6a8494bc2573d4852844f14898bb0657d3
Sergei <nb.b.2015@bk.ru>
2024-06-20 15:26:53 +0300

    1

--- TaskManagement.Desktop/Models/TaskModel.cs
+++ TaskManagement.Desktop/Models/TaskModel.cs
@@ -5,25 +5,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TaskManagement.DataBase.Entities;
-using Task = TaskManagement.DataBase.Entities.Task;
 
 namespace TaskManagement.Desktop.Models
 {
     public class TaskModel
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-		public DateTime Created { get; set; }
-		public DateTime Updated { get; set; }
-		public bool Deleted { get; set; }
+        public DateTime Created { get; set; }
 
-		public DateTime StartDate { get; set; }
+        public DateTime StartDate { get; set; }
 
         public DateTime? EndDate { get; set; }
         public DateTime Deadline { get; set; }
-        public int UserId { get;set; } 
-        public int ProjectId { get; set; }
+
         public User User { get; set; }
 
         public Status Status { get; set; }
@@ -32,7 +27,6 @@ namespace TaskManagement.Desktop.Models
         {
             return new TaskModel()
             {
-                Id = task.Id,
                 Created = task.Created,
                 StartDate = task.StartDate,
                 EndDate = task.EndDate,
@@ -40,7 +34,6 @@ namespace TaskManagement.Desktop.Models
                 Name = task.Name,
                 Description = task.Description,
                 User = new User().ToModel(task.User),
-                UserId = task.UserId
             };
         }
 
@@ -48,7 +41,6 @@ namespace TaskManagement.Desktop.Models
         {
             return new TaskModel()
             {
-                Id = historyChangeStatusTask.TaskId,
                 Created = historyChangeStatusTask.Task.Created,
                 StartDate = historyChangeStatusTask.Task.StartDate,
                 EndDate = historyChangeStatusTask.Task.EndDate,
@@ -56,29 +48,10 @@ namespace TaskManagement.Desktop.Models
                 Name = historyChangeStatusTask.Task.Name,
                 Description = historyChangeStatusTask.Task.Description,
                 User = new User().ToModel(historyChangeStatusTask.Task.User),
-                Status = historyChangeStatusTask.Status,
-                UserId = new User().ToModel(historyChangeStatusTask.Task.User).Id,
-                ProjectId = historyChangeStatusTask.Task.ProjectId
-			};
+                Status = historyChangeStatusTask.Status
+            };
         }
 
-        public DataBase.Entities.Task ToDbModel()
-        {
-			return new Task()
-			{
-                Id = this.Id,
-				Created = this.Created,
-				Updated = this.Updated,
-				Deleted = this.Deleted,
-				StartDate = this.StartDate,
-				EndDate = this.EndDate,
-				Deadline = this.Deadline,
-				Name = this.Name,
-				Description = this.Description,
-				UserId = this.UserId,
-				ProjectId = this.ProjectId,
-			};
-
-		}
-	}
+ 
+    }
 }
